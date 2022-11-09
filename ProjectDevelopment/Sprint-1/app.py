81082c6f483ad2e965c0f0c604a21fa365956a82
samapth-ops <sampathkaali002@gmail.com>
2022-11-09 19:28:28 +0530

    sprint-1 initiated

---
+++ ProjectDevelopment/Sprint-1/app.py
@@ -0,0 +1,20 @@
+from distutils.log import debug
+from flask import Flask, render_template
+import ibm_db
+
+try:
+    conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;UID=mkb48397;PWD=4joZwnJswX0BRnwT",'','')
+    print(conn)
+    print("connection successfull")
+except:
+    print("Error in connection, sqlstate = ")
+    errorState = ibm_db.conn_error()
+    print(errorState)
+
+app = Flask(__name__,static_url_path='/static')
+
+@app.route('/')
+def home():
+    return "hello flask"
+if __name__ == '__main__':
+    app.run(debug=True)
